d385725cf17d4de252c8b9bc10e466c6e83166df
sukhh002 <sukhh002naver.com>
2017-09-14 10:49:20 +0900

    18

--- app/src/main/java/edu/android/and57_git/MainActivity.java
+++ app/src/main/java/edu/android/and57_git/MainActivity.java
@@ -9,5 +9,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void test(){
+        //우끼끼끼끼끼
     }
 }
